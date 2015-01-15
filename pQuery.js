0010188deb9239c4858e0fc8a4ffc1f3a5a86a74
Vodzo <vodzo@zelic.info>
2015-01-15 21:32:29 +0100

    fixed chaining

--- pQuery.js
+++ pQuery.js
@@ -4,10 +4,10 @@ var pQuery = {
 		for (x in string)
 		{
 			var object = string[x];
-			this.parse_object(object);
+			this.parse_object(object, $(object.selector));
 		}
 	},
-	parse_object: function (object)
+	parse_object: function (object, that)
 	{
 		for(y in object.method)
 			{
@@ -15,19 +15,25 @@ var pQuery = {
 			if(object.arguments[y].length > 0)
 				{
 				var arguments = [];
+				var selector = $(object.selector);
+				if(that !== undefined)
+					{
+					selector = that;
+					}
+				
 				for(z in object.arguments[y])
 					{
 					var child_object = object.arguments[y][z];
 					if(child_object.arguments !== undefined && child_object.arguments.length > 0)
 						{
-						arguments.push(this.parse_object(child_object));		
+						arguments.push(this.parse_object(child_object, selector));		
 						}
 					else
 						{
 						arguments.push(child_object);	
 						}
 					}
-				$(object.selector)[methodName].apply($(object.selector), arguments);
+				selector[methodName].apply(selector, arguments);
 				}
 			else
 				{
